3e067f64bdba27a482985f334bca5dc56fa4a969
dddragons0803 <devanshgulati2002@gmail.com>
2024-06-28 20:07:56 +0530

    swagger url

--- swagger.js
+++ swagger.js
@@ -22,7 +22,7 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 const setupSwagger = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
 module.exports = setupSwagger;
