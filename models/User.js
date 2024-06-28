2fa2ab7c08d49f4f9824cbb6dd3a47f54d8b2e22
dddragons0803 <devanshgulati2002@gmail.com>
2024-06-28 16:16:47 +0530

    adding schema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isVerified: { type: Boolean, default: false },
+    username: 
+    { type: String,
+         required: true,
+          unique: true },
+    email: { type: String, 
+        required: true, 
+        unique: true },
+    password: { type: String, 
+        required: true },
+    isVerified: { type: Boolean,
+         default: false },
     otp: { type: String },
     location: String,
     age: Number,
